cac870539931ea01c99174cc729ee674f61ec0bc
adewaleK <bisdam91@gmail.com>
2020-08-05 16:42:12 +0100

    adds user_id column to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :author, presence: true
+  validates :body, presence: true
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_142502) do
+ActiveRecord::Schema.define(version: 2020_08_05_152558) do
 
   create_table "posts", force: :cascade do |t|
     t.string "author"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
