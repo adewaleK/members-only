e0d908277024373f4e462ab0991892f75ce26144
adewaleK <bisdam91@gmail.com>
2020-08-05 16:04:38 +0100

    redirects home page to the root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
   devise_for :users
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
