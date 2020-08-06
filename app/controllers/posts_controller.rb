e3dc5a20dfaff10f94b89fec179027c5e42728d2
adewaleK <bisdam91@gmail.com>
2020-08-06 01:30:11 +0100

    adds new.html.erb view to create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,20 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  
+
   def new
+    @post = current_user.posts.build
   end
 
   def create
+    @post = current_user.posts.build(book_params)
   end
 
   def index
+    @posts = Post.all
   end
+
+  private
+    def book_params
+      params.require(:post).permit(:author, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/index'
+  #get 'posts/new'
+  #get 'posts/create'
+  #get 'posts/index'
+  resources :posts, only: [:new, :create, :index]
   devise_for :users
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
